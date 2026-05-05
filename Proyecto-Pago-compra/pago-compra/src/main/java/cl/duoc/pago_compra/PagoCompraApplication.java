58f8ce0555b03c428c6192b353538bf9a1e2be38
seiiwl <69690921+Benja-ignacio@users.noreply.github.com>
2026-05-05 11:04:56 -0400

    se generaron proyectos

---
+++ Proyecto-Pago-compra/pago-compra/src/main/java/cl/duoc/pago_compra/PagoCompraApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.pago_compra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagoCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagoCompraApplication.class, args);
+	}
+
+}
---
+++ Proyecto-Pago-compra/pago-compra/src/test/java/cl/duoc/pago_compra/PagoCompraApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.pago_compra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagoCompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
