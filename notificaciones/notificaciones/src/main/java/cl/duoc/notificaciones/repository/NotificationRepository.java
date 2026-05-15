314615deec6d163fc867f0626f78c36e98b644bf
seiiwl <69690921+Benja-ignacio@users.noreply.github.com>
2026-05-15 16:43:36 -0400

    Se modifico el repository

--- notificaciones/notificaciones/src/main/java/cl/duoc/notificaciones/repository/NotificationRepository.java
+++ notificaciones/notificaciones/src/main/java/cl/duoc/notificaciones/repository/NotificationRepository.java
@@ -1,6 +1,7 @@
 package cl.duoc.notificaciones.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,11 +9,14 @@ import org.springframework.stereotype.Repository;
 import cl.duoc.notificaciones.model.Notification;
 
 @Repository
-public interface notificationRepository extends JpaRepository<Notification, Long> {
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     // Buscar todas las notificaciones de un usuario
     List<Notification> findByUserId(Long userId);
 
     // Buscar notificaciones de un usuario filtrando por leída/no leída
     List<Notification> findByUserIdAndRead(Long userId, Boolean read);
+
+    // buscar notificacion por id 
+    Optional<Notification> findById(Long id);
 }
