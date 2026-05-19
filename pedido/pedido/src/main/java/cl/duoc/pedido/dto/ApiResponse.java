d5357d944c185c852ac27285ece83311e059bc40
seiiwl <69690921+Benja-ignacio@users.noreply.github.com>
2026-05-19 01:26:45 -0400

    se agrego apiResponse

--- pedido/pedido/src/main/java/cl/duoc/pedido/dto/ApiResponse.java
+++ pedido/pedido/src/main/java/cl/duoc/pedido/dto/ApiResponse.java
@@ -2,11 +2,12 @@ package cl.duoc.pedido.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ApiResponse<T> {
-
     private int code;
     private String message;
     private T data;
