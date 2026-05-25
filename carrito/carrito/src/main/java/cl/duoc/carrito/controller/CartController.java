a9cae608a2b92e357520fc91d3b2c76f8db06ed7
seiiwl <69690921+Benja-ignacio@users.noreply.github.com>
2026-05-25 01:40:38 -0400

    se cambiaron las configuraciones de la bd y dependencias para testeos rapidos

--- carrito/carrito/src/main/java/cl/duoc/carrito/controller/CartController.java
+++ carrito/carrito/src/main/java/cl/duoc/carrito/controller/CartController.java
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/cart")
+@RequestMapping("/api/v1/cart")
 @RequiredArgsConstructor
 public class CartController {
 
