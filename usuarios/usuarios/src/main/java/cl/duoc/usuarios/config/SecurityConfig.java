83db13e8cbe495c823faca43beeb5fe540e5f8ba
seiiwl <69690921+Benja-ignacio@users.noreply.github.com>
2026-05-28 17:04:44 -0400

    Configuracion de Eureka y Dockerfile de los servicios

--- usuarios/usuarios/src/main/java/cl/duoc/usuarios/config/SecurityConfig.java
+++ usuarios/usuarios/src/main/java/cl/duoc/usuarios/config/SecurityConfig.java
@@ -1,20 +1,29 @@
-package cl.duoc.usuarios.securityConfig;
+package cl.duoc.usuarios.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
+    @Order(1)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("api/v1/public/**")
+        return http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // Elimina el JSESSIONID
             )
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            )
+            .build();
     }
 
 }
