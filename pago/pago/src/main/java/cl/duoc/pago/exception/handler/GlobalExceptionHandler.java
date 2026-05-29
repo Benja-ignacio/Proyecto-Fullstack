17585e02ce041ad37f8e90ce1d3b1f001fde6f65
seiiwl <69690921+Benja-ignacio@users.noreply.github.com>
2026-05-29 12:25:48 -0400

    Handler global y excepciones personalizadas

---
+++ pago/pago/src/main/java/cl/duoc/pago/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,81 @@
+package cl.duoc.pago.exception.handler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import cl.duoc.pago.exception.custom.PaymentNotFoundException;
+import cl.duoc.pago.exception.custom.PaymentRejectedException;
+
+public class GlobalExceptionHandler {
+
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+            
+    @ExceptionHandler(Exception.class) // EXCEPCIONES GLOBALES 
+    public ResponseEntity<Map<String, String>> handleAll(Exception ex) {
+
+        // log interno completo
+        logger.error("Error inesperado", ex);
+
+        // respuesta limpia al cliente
+        Map<String, String> response = Map.of(
+                "error", "Error interno del servidor"
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class) // ERRORES DE VALID
+    public ResponseEntity<Map<String, String>> validExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errores = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errores.put(error.getField(), error.getDefaultMessage());
+        });
+
+        logger.warn("Error de validación: {}", ex.getMessage());
+        
+        return new ResponseEntity<>(errores, HttpStatus.BAD_REQUEST);
+    }
+
+
+    // NOTA usar ex en ves de ex.getMessage cuando se necesite ver los logs completos. 
+
+    @ExceptionHandler(PaymentNotFoundException.class) 
+    public ResponseEntity<Map<String, String>> PaymentNotFoundException(PaymentNotFoundException ex) {
+
+        logger.warn("Pago no encontrado: {}", ex.getMessage()); // LOGS QUE SOLO SE PUEDE VER INTERNAMENTE
+
+        Map<String, String> error = Map.of(
+            "error", ex.getMessage() // ERROR QUE VE EL FRONTEND
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(PaymentRejectedException.class) 
+    public ResponseEntity<Map<String, String>> PaymentRejectedException(PaymentRejectedException ex) {
+
+        logger.warn("Pago rechazado: {}", ex.getMessage()); // LOGS QUE SOLO SE PUEDE VER INTERNAMENTE
+
+        Map<String, String> error = Map.of(
+            "error", ex.getMessage() // ERROR QUE VE EL FRONTEND
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+
+}
