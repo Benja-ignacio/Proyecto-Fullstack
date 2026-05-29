c9c6a48a17ab9a789aa90c35cbae3d26d99c3a57
seiiwl <69690921+Benja-ignacio@users.noreply.github.com>
2026-05-29 16:01:06 -0400

    se agrego la anotacion @RestControllerAdvice

--- pago/pago/src/main/java/cl/duoc/pago/exception/handler/GlobalExceptionHandler.java
+++ pago/pago/src/main/java/cl/duoc/pago/exception/handler/GlobalExceptionHandler.java
@@ -9,10 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import cl.duoc.pago.exception.custom.PaymentNotFoundException;
 import cl.duoc.pago.exception.custom.PaymentRejectedException;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
 
