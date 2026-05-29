e1fa9f7e8774c244a182199e782bd4d64ea4cbde
seiiwl <69690921+Benja-ignacio@users.noreply.github.com>
2026-05-29 15:53:09 -0400

    Se eliminaron imports no usados

--- descuentos/descuentos/src/main/java/cl/duoc/descuentos/controller/DIscountController.java
+++ descuentos/descuentos/src/main/java/cl/duoc/descuentos/controller/DIscountController.java
@@ -7,7 +7,6 @@ import cl.duoc.descuentos.dto.ApiResponse;
 import cl.duoc.descuentos.dto.DiscountRequestDTO;
 import cl.duoc.descuentos.dto.DiscountResponseDTO;
 import cl.duoc.descuentos.dto.DiscountUsageResponseDTO;
-import cl.duoc.descuentos.model.Discount;
 import cl.duoc.descuentos.service.DiscountService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,8 +18,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
