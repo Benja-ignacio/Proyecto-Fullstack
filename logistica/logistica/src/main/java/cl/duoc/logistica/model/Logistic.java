6ce944291545d12f0a765ee6febe33434219e29a
seiiwl <69690921+Benja-ignacio@users.noreply.github.com>
2026-06-09 11:17:50 -0400

    se agrego @id, @generatedvalue

--- logistica/logistica/src/main/java/cl/duoc/logistica/model/Logistic.java
+++ logistica/logistica/src/main/java/cl/duoc/logistica/model/Logistic.java
@@ -1,14 +1,15 @@
 package cl.duoc.logistica.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import org.springframework.cglib.core.Local;
 
 import cl.duoc.logistica.enums.Status;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,7 +25,10 @@ import lombok.Setter;
 
 
 public class Logistic {
-    private Long id; // pk
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id; // 
 
     @Column(name = "order_id")
     private Long orderId; // referencia a order service
