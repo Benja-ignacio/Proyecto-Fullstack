f465ad9b3e3643fe83f5f2f4f9f1b4cbdc50cb0e
seiiwl <69690921+Benja-ignacio@users.noreply.github.com>
2026-06-18 21:49:28 -0400

    Config de seguridad

--- usuarios/usuarios/src/main/java/cl/duoc/usuarios/config/SecurityConfig.java
+++ usuarios/usuarios/src/main/java/cl/duoc/usuarios/config/SecurityConfig.java
@@ -3,15 +3,24 @@ package cl.duoc.usuarios.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import cl.duoc.usuarios.security.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
     @Bean
     @Order(1)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,8 +30,26 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // Elimina el JSESSIONID
             )
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
+                // rutas publicas
+                .requestMatchers(
+                    "/api/v1/auth/login", 
+                    "/api/v1/auth/register",
+                    "/actuator/**",
+                    "/v3/api-docs/**",
+                    "/swagger-ui/**",
+                    "/swagger-ui.html",
+                    "/swagger-ui/index.html",
+                    "/webjars/**").permitAll()
+
+                // rutas solo admin
+                .requestMatchers(HttpMethod.PATCH, "/api/v1/users/status/**").hasRole("ADMIN")
+                .requestMatchers("/api/v1/users/list").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/api/v1/users/{id}").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/api/v1/users/exists/{userId}").hasRole("ADMIN")
+                // todo lo demas requiere autenticacion 
+                .anyRequest().authenticated()
             )
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
     }
 
