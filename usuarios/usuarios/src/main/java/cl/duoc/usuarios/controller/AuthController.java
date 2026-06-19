7b7eb73804df72952e2895b90d837dea3c87997c
seiiwl <69690921+Benja-ignacio@users.noreply.github.com>
2026-06-18 21:49:52 -0400

    Se corrijio requestmapping - faltaba un slash al inicio

--- usuarios/usuarios/src/main/java/cl/duoc/usuarios/controller/AuthController.java
+++ usuarios/usuarios/src/main/java/cl/duoc/usuarios/controller/AuthController.java
@@ -1,11 +1,9 @@
 package cl.duoc.usuarios.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import cl.duoc.usuarios.dto.requests.LoginRequestDTO;
@@ -20,11 +18,12 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/auth")
+@RequestMapping("/api/v1/auth")
 public class AuthController {
     private final AuthService authService;
 
-        //endpoint para registrar U getByusuario
+    //ACCESO: PUBLICO
+    //endpoint para registrar un usuario
     @PostMapping("/register")
     @Operation(summary = "Registrar un nuevo usuario", description = "Permite registrar un nuevo usuario pidiendo user, password, adress y email")
     public ResponseEntity<ApiResponse<RegisterResponseDTO>> register(
@@ -39,7 +38,8 @@ public class AuthController {
         return ResponseEntity.ok(response);
     }
 
-        //endpoint para login 
+    //ACCESO: PUBLICO
+    //endpoint para login 
     @PostMapping("/login")
     @Operation(summary = "Login del usuario", description = "Permite logear a un usuario pidiendo su username y password")
     public ResponseEntity<ApiResponse<String>> login (@Valid @RequestBody LoginRequestDTO loginDTO) {
@@ -53,28 +53,6 @@ public class AuthController {
             ApiResponse<String> response = 
                     new ApiResponse<String>(401, "Credenciales invalidas", null);
             return ResponseEntity.status(401).body(response);
-        }
-
-        
+        }  
     }
-
-        //endpoint para validar token 
-    @GetMapping("/validate")
-    @Operation(summary = "Validar token", description = "Permite validar un token, requiere el token como parametro")
-    public ResponseEntity<ApiResponse<String>> validateToken (@RequestParam String token) {
-        boolean valid = authService.validateToken(token);
-
-        if (valid) {
-            ApiResponse<String> response = 
-                    new ApiResponse<String>(200, "Token valido", "OK");
-            
-            return ResponseEntity.ok(response);
-        } else {
-            ApiResponse<String> response =
-                    new ApiResponse<String>(401, "Token invalido", null);
-
-            return ResponseEntity.status(401).body(response);
-        }
-    }
-
 }
