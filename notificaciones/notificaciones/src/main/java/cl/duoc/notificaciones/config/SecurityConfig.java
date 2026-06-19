69b19260a0ae9b442553c3d054d57edd2d8bf9d4
seiiwl <69690921+Benja-ignacio@users.noreply.github.com>
2026-06-18 21:46:31 -0400

    Configuracion de seguridad

--- notificaciones/notificaciones/src/main/java/cl/duoc/notificaciones/config/SecurityConfig.java
+++ notificaciones/notificaciones/src/main/java/cl/duoc/notificaciones/config/SecurityConfig.java
@@ -7,10 +7,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import cl.duoc.notificaciones.security.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpMethod;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
     @Bean
     @Order(1)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -20,8 +30,26 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // Elimina el JSESSIONID
             )
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
+                
+                // rutas publicas
+                .requestMatchers(             
+                "/actuator/**",
+                "/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html",
+                "/swagger-ui/index.html",
+                "/webjars/**").permitAll()
+
+                // solo admin
+                .requestMatchers( HttpMethod.GET, "/api/v1/notification/*").hasRole("ADMIN")
+                .requestMatchers( HttpMethod.GET, "/api/v1/notification/user/*").hasRole("ADMIN")
+                .requestMatchers( HttpMethod.POST, "/api/v1/notification/create").hasRole("ADMIN")
+                .requestMatchers( HttpMethod.POST, "/api/v1/notification/delete/*").hasRole("ADMIN")
+        
+                // todo lo demas autenticado
+                .anyRequest().authenticated()
             )
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
     }
 
