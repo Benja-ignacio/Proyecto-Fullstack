780c0f56f1b810349e4ad6d21547b8b49d679322
seiiwl <69690921+Benja-ignacio@users.noreply.github.com>
2026-06-22 10:05:28 -0400

    se corrijeron errores

--- eureka-server/src/main/java/cl/duoc/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/cl/duoc/eureka_server/EurekaServerApplication.java
@@ -4,15 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-<<<<<<< HEAD
-@SpringBootApplication
-@EnableEurekaServer
-public class EurekaServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
-	}
-=======
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
@@ -20,6 +11,5 @@ public class EurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
->>>>>>> eliascarcamo
 
 }
